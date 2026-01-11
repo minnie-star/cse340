c4a7935ad00692b1c436670ce38c5eade141c9d2
minnie-star <minenhleminnie8@gmail.com>
2026-01-11 21:47:21 +0200

    week01 assignment

--- newrepo-main/server.js
+++ newrepo-main/server.js
@@ -5,15 +5,28 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * Views Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
+app.use(require("./routes/static"))
+// Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
