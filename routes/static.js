e597044b51db8881d849ada816d2317c7b1327e2
minnie-star <minenhleminnie8@gmail.com>
2026-01-14 23:22:58 +0200

    week01

--- routes/static.js
+++ routes/static.js
@@ -1,12 +1,23 @@
+// const express = require('express');
+// const router = express.Router();
+
+// //Static Routes
+// //Set up "public" folder / subfolders for static files
+// router.use(express.static("public"));
+// router.use("/css", express.static(__dirname + "public/css"));
+// router.use("/js", express.static(__dirname + "public/js"));
+// router.use("/images", express.static(__dirname + "public/images"));
+
+// module.exports = router;
+
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
-//Static Routes
-//Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
+// Serve static files from "public" and its subfolders
+router.use(express.static(path.join(__dirname, '../public')));
+router.use('/css', express.static(path.join(__dirname, '../public/css')));
+router.use('/js', express.static(path.join(__dirname, '../public/js')));
+router.use('/images', express.static(path.join(__dirname, '../public/images')));
 
 module.exports = router;
-
